8698b3e9232525f2d338bec20b469ea8131002a6
Clara Liu <clara_lzr@hotmail.com>
2020-10-02 16:25:51 +0100

    update README

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # A collection of python functions for praat object manipulation and processing 
 
 ##Table of contents
-[Get duration df from textGrid]()
+[Get duration data from textGrid file](https://github.com/Clara-liu/python_praat#Get-duration-data-from-textGrid-file)
+
+
+## Get duration data from textGrid file
+
+The get_duration file contains a python function using the library textgrid and pandas. The textGrid file should contained intervals labelled with categorical values seperated by space. For example, if two categorical variables are needed: vowel (a ae e i o u) and length (L S), one of your label could be "ae L".  
+Example:  
+
+```Python
+# The order of the variable needs to be the same as your label
+category_list = ['Vowel', 'Length']
+
+df = get_duration_df('filePath', category_list)
+```
--- get_duration.py
+++ get_duration.py
@@ -5,18 +5,23 @@ def get_duration_df(path, category_list):
     import pandas as pd
     # read the textgrid object
     grid = textgrid.TextGrid.fromFile(path)
-    # go through all the intervals
-    int_num = grid[0].__len__()
     # initialise duration df
     category_list.append('Duration')
     data = {c:[] for c in category_list}
+    # go through all the intervals
+    int_num = grid[0].__len__()
     for i in range(int_num):
         label = grid[0][i].mark
+        # get data from intervals that contain label
         if len(label) > 0:
+            # create list that contains the categorical values
             label = label.split()
+            # get duration
             dur = grid[0][i].duration()
             data['Duration'].append(dur)
+            # append categorical data value to df dict
             for l in range(len(label)):
                 data[category_list[l]].append(label[l])
+    # transform to pd df
     df = pd.DataFrame.from_dict(data)
     return df
